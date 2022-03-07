3f56a611238114376bba1873e7c4caa1c29180a6
SememN <lutovinovsemen@gmail.com>
2022-03-07 23:20:39 +0500

    main page (without links)

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, this is main KR page')
+    return render(request, 'main/main_page.html')
