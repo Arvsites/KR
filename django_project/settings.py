2e7aa07575f6fab845ab20f328d2c50aa6e470f2
SememN <lutovinovsemen@gmail.com>
2022-03-10 17:44:43 +0500

    add db settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kr',
+        'USER': 'postgres',
+        'PASSWORD': 'FjhfNB693>M',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
