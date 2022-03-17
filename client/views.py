ce51469ca7d49d9df4fb68e828092f1826d6e2f1
SememN <lutovinovsemen@gmail.com>
2022-03-17 11:43:15 +0500

    bugfix

--- client/views.py
+++ client/views.py
@@ -13,6 +13,8 @@ def client_page(request):
         return HttpResponse(user)
     else:
         grafana_data = services.get_data(user)
+        if len(grafana_data) == 1:
+            return HttpResponse(f'У пользователя {username} с id {user.id} нет кондиционеров')
         return render(request, 'client/client.html', {'user': user, 'grafana_data': grafana_data})
 
 
