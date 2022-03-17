34c1345e82604316efe64e3dacded17d54e05d87
SememN <lutovinovsemen@gmail.com>
2022-03-17 11:45:54 +0500

    bugfix

--- client/services.py
+++ client/services.py
@@ -30,6 +30,8 @@ def get_data(user):
 
     if user_id == 1:
         for client in User.objects.all():
+            if client.id == 1:
+                continue
             airconds_count = AircondData.objects.filter(client_login=client.id).first().airconds_count
             for i in range(1, airconds_count * 2 + 1):
                 grafana_data_list.append(f"http://37.140.197.191:3000/d-solo/bDeXhSEnk/user{str(client.id)}?orgId=2&from"
