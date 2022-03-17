978e5ea2e4de927b85ee710495cf9afa12fa46ed
SememN <lutovinovsemen@gmail.com>
2022-03-17 13:02:51 +0500

    del unnecessary fields

--- client/models.py
+++ client/models.py
@@ -11,8 +11,6 @@ class AircondData(models.Model):
     t4 = models.SmallIntegerField(blank=True, null=True)
     t5 = models.SmallIntegerField(blank=True, null=True)
     pressure = models.SmallIntegerField(blank=True, null=True)
-    c1 = models.SmallIntegerField(blank=True, null=True)
-    c2 = models.SmallIntegerField(blank=True, null=True)
     cond_id = models.SmallIntegerField(blank=True, null=True)
     current = models.SmallIntegerField(blank=True, null=True)
     client_login = models.ForeignKey(User, on_delete=models.CASCADE)
