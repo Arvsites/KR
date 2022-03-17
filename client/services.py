7f21deb7d6534fe053c1051c619d1f635bd888e7
SememN <lutovinovsemen@gmail.com>
2022-03-17 13:09:55 +0500

    debug

--- client/services.py
+++ client/services.py
@@ -32,7 +32,9 @@ def get_data(user):
         for client in User.objects.all():
             if client.id == 1:
                 continue
-            airconds_count = AircondData.objects.filter(client_login_id=client.id).first().airconds_count
+            client_id = AircondData.objects.filter(client_login_id=client.id)
+            first_data_object = client_id.first()
+            airconds_count = first_data_object.airconds_count
             for i in range(1, airconds_count * 2 + 1):
                 grafana_data_list.append(f"http://37.140.197.191:3000/d-solo/bDeXhSEnk/user{str(client.id)}?orgId=2&from"
                                          f"=now-7d&to=now&theme=dark&viewPanel={str(i)}")
