5ba48dadbc2a78b5fda76dad729e0afa0100a465
SememN <lutovinovsemen@gmail.com>
2022-03-25 12:17:23 +0500

    bugfix

--- django_project/local_settings.py
+++ django_project/local_settings.py
@@ -10,17 +10,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '37.140.197.191', '2a00:f940:2:4:2::2619', '37-140-197-191.cloudvps.regruhosting.ru',
                  'multimer.ru']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kr',
-        'USER': 'postgres',
-        'PASSWORD': 'FjhfNB693>M',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
