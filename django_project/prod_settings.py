446f8d1e7b5a48b645516c6f64cdffae90eb3ff5
SememN <lutovinovsemen@gmail.com>
2022-03-26 22:30:22 +0500

    add favicon

--- django_project/prod_settings.py
+++ django_project/prod_settings.py
@@ -10,8 +10,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '194.67.121.74', '2a00:f940:2:4:2::2619', '37-140-197-191.cloudvps.regruhosting.ru',
                  'multimer.ru']
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]
+]'''
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
