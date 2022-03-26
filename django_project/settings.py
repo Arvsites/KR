18aa00a6610a1a397ff1aab4282dc420a605f843
SememN <lutovinovsemen@gmail.com>
2022-03-26 22:14:01 +0500

    add pwa

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,7 +123,6 @@ CSRF_TRUSTED_ORIGINS = ['https://multimer.ru', 'https://127.0.0.1']
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 365 * 10
 
 # PWA
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
 
 PWA_APP_NAME = 'multimer'
 PWA_APP_DESCRIPTION = 'multimer'
