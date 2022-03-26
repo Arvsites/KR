0d6ae746d3f5d44a14c5ef32036c07c7d7e94065
SememN <lutovinovsemen@gmail.com>
2022-03-26 22:20:27 +0500

    add favicon

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('client/', include('client.urls')),
     path('', include('pwa.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
