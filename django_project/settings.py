6118bb722ee0f1668f4bc7e626c7dcfc69ed0c0e
SememN <lutovinovsemen@gmail.com>
2022-03-27 13:06:57 +0500

    service worker

--- django_project/settings.py
+++ django_project/settings.py
@@ -115,6 +115,38 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# PWA
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'sw.js')
+
+PWA_APP_NAME = 'Multimer'
+PWA_APP_DESCRIPTION = "Multimer"
+PWA_APP_THEME_COLOR = '#0A0302'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/pwa-images/android-icon-192x192.png',
+        'sizes': '192x192'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/pwa-images/apple-icon-180x180.png',
+        'sizes': '180x180'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/pwa-images/splash-512x512.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'ru-RU'
 
 # Cookies
 CSRF_TRUSTED_ORIGINS = ['https://multimer.ru', 'https://127.0.0.1']
