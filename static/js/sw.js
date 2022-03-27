36bf7a1e490fc1b99a7c354996b90a28681c37b0
SememN <lutovinovsemen@gmail.com>
2022-03-27 12:22:29 +0500

    service worker

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
 // регистрирует скрипт sw в поддерживаемых браузерах
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://multimer.ru/static/js/sw.js', { scope: '/' }).then(() => {
+    navigator.serviceWorker.register('https://multimer.ru/static/js/sw.js', { scope: '/../..' }).then(() => {
       console.log('Service Worker registered successfully.');
     }).catch(error => {
       console.log('Service Worker registration failed:', error);
