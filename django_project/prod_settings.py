a608c1ee3ffdff58892612d7b171d0808f6340d5
SememN <lutovinovsemen@gmail.com>
2022-05-26 22:42:03 +0500

    bugfix

--- django_project/prod_settings.py
+++ django_project/prod_settings.py
@@ -7,8 +7,4 @@ SECRET_KEY = 'IYjmGfjSLygdeJZAOPhkqtedDVScDNyOnHawoLQZLWcPhDpVtYflrkRVHJXlYvZmE'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '194.67.121.74', '2a00:f940:2:4:2::2619', '37-140-197-191.cloudvps.regruhosting.ru',
-                 'multimer.ru']
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
