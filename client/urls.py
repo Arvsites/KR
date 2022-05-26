2de30865254631b4f756442047ac01059d8ca443
SememN <lutovinovsemen@gmail.com>
2022-05-26 21:29:57 +0500

    del trailing '/'

--- client/urls.py
+++ client/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'client'
 urlpatterns = [
     path('', views.client_page, name='client_page'),
-    path('/<int:days_to_show>', views.client_page, name='client_page'),
+    path('<int:days_to_show>', views.client_page, name='client_page'),
     path('login_page', views.login_page, name='login_page'),
     path('login', views.login, name='login')
 ]
