ee880f77eb22e5ae082528bc57782c32ca3d795f
SememN <lutovinovsemen@gmail.com>
2022-05-29 23:02:00 +0500

    bugfix

--- db_loader/error_hadler.py
+++ db_loader/error_hadler.py
@@ -2,42 +2,41 @@ import paho.mqtt.client as mqtt
 
 import config
 
-
 # check data
 MAX_VALUES = {
-    't1': 35,
-    't2': 35,
-    't3': 35,
-    't4': 35,
-    't5': 35,
-    'pressure': 100,
-    'current': 100,
+    't1' : 35,
+    't2' : 35,
+    't3' : 35,
+    't4' : 35,
+    't5' : 35,
+    'b1' : 100,
+    'i1' : 100,
 }
 
 MIN_VALUES = {
-    't1': -30,
-    't2': -30,
-    't3': -30,
-    't4': -30,
-    't5': -30,
-    'pressure': 50,
-    'current': 50,
+    't1' : -30,
+    't2' : -30,
+    't3' : -30,
+    't4' : -30,
+    't5' : -30,
+    'b1' : 50,
+    'i1' : 50,
 }
 
 
-def analyze_data(data: dict):
+def analyze_data(data: dict) :
     """Analyzes data from sensors and detects if there any errors. If some value is more than MAX_VALUE or less than
     MIN_VALUES """
     print("analyze data")
     errors = []
 
-    for key in data.keys():
-        if key == 'time' or key == 'cond_id' or key == 'client_id' or key == 'client_id' or key == 'telegram_chat_id' or key == 'airconds_count':
+    for key in data.keys() :
+        if key == 'time' or key == 'cond_id' or key == 'client_id' or key == 'telegram_chat_id' or key == 'airconds_count':
             continue
-        if int(data[key]) > MAX_VALUES[key] or int(data[key]) < MIN_VALUES[key]:
+        if int(data[key]) > MAX_VALUES[key] or int(data[key]) < MIN_VALUES[key] :
             errors.append(f'Датчик {key} выдал аномальное значение {data[key]}! Проверьте, всё ли в порядке.')
 
-    if not errors:
+    if not errors :
         return
 
     return [data['telegram_chat_id'], errors]
@@ -52,9 +51,9 @@ client.username_pw_set(config.BROKER_LOGIN, config.BROKER_PASSWORD)
 client.connect(broker_address, port=config.BROKER_PORT)
 
 
-def publish_errors(errors: list):
+def publish_errors(errors: list) :
     """Publish errors to mqtt broker"""
     # errors type - [telegram_chat_id, [errors]]
-    for error in errors[1]:
+    for error in errors[1] :
         print("sending error")
-        client.publish("/errors", {errors[0]: error})
+        client.publish("/errors", {errors[0] : error})
