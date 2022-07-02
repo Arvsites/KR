ffd0516466606720c83bf1fdc3d27312443245ed
SememN <lutovinovsemen@gmail.com>
2022-07-02 16:47:56 +0500

    fix auth bug

--- client/views.py
+++ client/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from . import services
 
 
-def client_page(request, days_to_show=''):
-    user = request.user
+def client_page(request, user, days_to_show=''):
     grafana_data = services.get_data(user, days=days_to_show)
     return render(request, 'client/client.html', {'user': user, 'grafana_data': grafana_data})
 
@@ -17,5 +16,5 @@ def login(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
 
-    services.signin(request, username, password)
-    return client_page(request)
+    user = services.signin(request, username, password)
+    return client_page(request, user)
