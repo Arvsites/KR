a87cf0568db0dec5a035f146d40ba8e2dc173a20
Dementr21 <105007100+Dementr21@users.noreply.github.com>
2022-10-05 21:54:29 +0500

    Create login.js

---
+++ static/client/js/login.js
@@ -0,0 +1,10 @@
+let username = document.getElementById('user');
+let password = document.getElementById('pass');
+
+function setCookies(params) {
+    let user = username.value;
+    let pass = password.value;
+
+    document.cookie = `username=${user}; path=/; `;
+    document.cookie = `password=${pass}; path=/; `;
+}
