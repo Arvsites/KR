8c69371ee3b1a811a0ae4a2b6dc40be9f80e1c06
Dementr21 <105007100+Dementr21@users.noreply.github.com>
2022-10-06 21:50:17 +0500

    Update client.js

--- static/client/js/client.js
+++ static/client/js/client.js
@@ -1,11 +1,9 @@
 let workingHref = document.getElementById("hidden_button");
-console.log("works")
 
 function delCookies() {
-    alert(workingHref.href);
          
     document.cookie = `username=${user}; path=/; max-age=0;`;
     document.cookie = `password=${pass}; path=/; max-age=0;`;
       
-    //window.location.href = event.target.href;
+    window.location.href = workingHref.href;
 }
