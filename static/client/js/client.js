663b7f0b16c38b543557cd9c8b0436f4d13187d1
Dementr21 <105007100+Dementr21@users.noreply.github.com>
2022-10-06 21:54:01 +0500

    Update client.js

--- static/client/js/client.js
+++ static/client/js/client.js
@@ -1,7 +1,9 @@
 let workingHref = document.getElementById("hidden_button");
 
 function delCookies() {
-         
+    let user = " ";
+    let pass = " ";
+        
     document.cookie = `username=${user}; path=/; max-age=0;`;
     document.cookie = `password=${pass}; path=/; max-age=0;`;
       
