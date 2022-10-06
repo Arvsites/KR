3a4edc343920e2f9da054ed643860779a857064a
Dementr21 <105007100+Dementr21@users.noreply.github.com>
2022-10-06 21:00:55 +0500

    Update client.js

--- static/client/js/client.js
+++ static/client/js/client.js
@@ -4,6 +4,5 @@ function delCookies(params) {
 
     document.cookie = `username=${user}; path=/; max-age=0;`;
     document.cookie = `password=${pass}; path=/; max-age=0;`;
-    alert(document.cookie);
-    window.location.href = 'https://ya.ru';
+    window.location.href = {% url 'client:logout' %};
 }
