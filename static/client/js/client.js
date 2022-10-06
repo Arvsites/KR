35d2e8c2428d6ea3ef44c45fbe360b488474b3e6
Dementr21 <105007100+Dementr21@users.noreply.github.com>
2022-10-06 21:18:00 +0500

    Update client.js

--- static/client/js/client.js
+++ static/client/js/client.js
@@ -1,10 +1,10 @@
 function delCookies(evt) {
+    alert(document.cookie);
     evt.preventDefault();
          
     document.cookie = `username=${user}; path=/; max-age=0;`;
     document.cookie = `password=${pass}; path=/; max-age=0;`;
-    alert(document.cookie);
-           
+      
     window.location.href = evt.target.href;
     }
 }
